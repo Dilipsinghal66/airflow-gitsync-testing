1758c0ec01f2e2c8c811d90248a5ec7ed62f2e12
Mrigesh Pokhrel <mrigeshpokhrel@gmail.com>
2019-03-18 19:12:23 +0530

    fix: automate dag creation, and filter patients from db itsel

--- reminders/task_reminder/dags.py
+++ reminders/task_reminder/dags.py
@@ -22,105 +22,21 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
-task_medical_arg_6_00 = Variable.get("task_medical_arg_6_00", deserialize_json=True)
-task_exercise_arg_7_30 = Variable.get("task_exercise_arg_7_30", deserialize_json=True)
-task_nutrition_arg_9_30 = Variable.get("task_nutrition_arg_9_30", deserialize_json=True)
-task_nutrition_arg_13_00 = Variable.get("task_nutrition_arg_13_00", deserialize_json=True)
-task_nutrition_arg_17_00 = Variable.get("task_nutrition_arg_17_00", deserialize_json=True)
-task_meditation_arg_22_00 = Variable.get("task_meditation_arg_22_00", deserialize_json=True)
-
-medical_dag_6_00 = DAG(
-    dag_id='medical_reminder_6_00',
-    default_args=default_args,
-    schedule_interval="0 6 * * *",
-    catchup=False
-)
-
-exercise_dag_7_30 = DAG(
-    dag_id='exercise_reminder_7_30',
-    default_args=default_args,
-    schedule_interval="30 7 * * *",
-    catchup=False
-)
-
-nutrition_dag_9_30 = DAG(
-    dag_id='nutrition_reminder_9_30',
-    default_args=default_args,
-    schedule_interval="30 9 * * *",
-    catchup=False
-)
-
-nutrition_dag_13_00 = DAG(
-    dag_id='nutrition_reminder_13_00',
-    default_args=default_args,
-    schedule_interval="0 13 * * *",
-    catchup=False
-)
-
-nutrition_dag_17_00 = DAG(
-    dag_id='nutrition_reminder_17_00',
-    default_args=default_args,
-    schedule_interval="0 17 * * *",
-    catchup=False
-)
-
-meditation_dag_22_00 = DAG(
-    dag_id='meditation_reminder_22_00',
-    default_args=default_args,
-    schedule_interval="0 22 * * *",
-    catchup=False
-)
-
-medical_reminder_6_00 = PythonOperator(
-    task_id="medical_6_30",
-    task_concurrency=1,
-    python_callable=send_reminder,
-    dag=medical_dag_6_00,
-    op_kwargs=task_medical_arg_6_00,
-    pool="task_reminder_pool"
-)
-
-exercise_reminder_7_30 = PythonOperator(
-    task_id="exercise_7_30",
-    task_concurrency=1,
-    python_callable=send_reminder,
-    dag=exercise_dag_7_30,
-    op_kwargs=task_exercise_arg_7_30,
-    pool="task_reminder_pool"
-)
-
-nutrition_reminder_9_30 = PythonOperator(
-    task_id="nutrition_9_30",
-    task_concurrency=1,
-    python_callable=send_reminder,
-    dag=nutrition_dag_9_30,
-    op_kwargs=task_nutrition_arg_9_30,
-    pool="task_reminder_pool"
-)
-
-nutrition_reminder_13_00 = PythonOperator(
-    task_id="nutrition_13_00",
-    task_concurrency=1,
-    python_callable=send_reminder,
-    dag=nutrition_dag_13_00,
-    op_kwargs=task_nutrition_arg_13_00,
-    pool="task_reminder_pool"
-)
-
-nutrition_reminder_17_00 = PythonOperator(
-    task_id="nutrition_17_00",
-    task_concurrency=1,
-    python_callable=send_reminder,
-    dag=nutrition_dag_17_00,
-    op_kwargs=task_nutrition_arg_17_00,
-    pool="task_reminder_pool"
-)
-
-meditation_reminder_22_00 = PythonOperator(
-    task_id="meditation_22_00",
-    task_concurrency=1,
-    python_callable=send_reminder,
-    dag=meditation_dag_22_00,
-    op_kwargs=task_meditation_arg_22_00,
-    pool="task_reminder_pool"
-)
+auto_dag_def = Variable.get("auto_dag", deserialize_json=True)
+for dag_def in auto_dag_def:
+    dag_name = dag_def.get("dag_name")
+    locals()[dag_name] = DAG(
+        dag_id=dag_def.get("dag_id"),
+        default_args=default_args,
+        schedule_interval=dag_def.get("schedule_interval"),
+        catchup=False
+    )
+    task_name = dag_def.get("task_name")
+    locals()[task_name] = PythonOperator(
+        task_id=dag_def.get("task_id"),
+        task_concurrency=1,
+        python_callable=send_reminder,
+        dag=locals().get(dag_name),
+        op_kwargs=dag_def.get("op_kwargs"),
+        pool="task_reminder_pool"
+    )
