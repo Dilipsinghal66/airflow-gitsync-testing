7dbb057883df27f4841dcd5e5c24ec0518303c03
Mrigesh Pokhrel <mrigeshpokhrel@gmail.com>
2019-03-28 21:32:52 +0530

    fix: dynamic reminder
    Signed-off-by: Mrigesh Pokhrel <mrigeshpokhrel@gmail.com>

---
+++ reminders/dynamic_reminders/reminder.py
@@ -0,0 +1,106 @@
+import json
+from datetime import date
+
+import requests
+from airflow.contrib.hooks.mongo_hook import MongoHook
+from airflow.hooks.http_hook import HttpHook
+from airflow.models import Variable
+from dateutil import parser
+
+
+def get_patient_days(patient):
+  days = None
+  user_flags = patient.get("userFlags", None)
+  if not user_flags:
+    return days
+  activation = user_flags.get("active", None)
+  if not activation:
+    return days
+  is_activated = activation.get("activated", False)
+  if not is_activated:
+    return days
+  activated_on = activation.get("activatedOn", None)
+  if not activated_on:
+    return days
+  if isinstance(activated_on, str):
+    activated_on = parser.parse(activated_on)
+  activated_date = activated_on.date()
+  today = date.today()
+  date_diff = today - activated_date
+  days = date_diff.days
+  days = days + 1
+  return days
+
+
+def get_parsed_resource_data(resource_url: str):
+  data = None
+  try:
+    response = requests.get(resource_url)
+    data = response.text
+    data = json.loads(data)
+  except Exception as e:
+    print(str(e))
+  return data
+
+
+def refresh_daily_message():
+  dynamic_message_url = "https://services.zyla.in/statemachine/scheduler/dynamic/message/today"
+  dynamic_message_list = get_parsed_resource_data(dynamic_message_url)
+  return dynamic_message_list
+
+
+def send_reminder(**kwargs):
+  time_data_url = "https://services.zyla.in/statemachine/scheduler/21:45/messages/4"
+  time_data = get_parsed_resource_data(time_data_url)
+  messages = time_data.get("messages")
+  dynamic_messages = refresh_daily_message()
+  message = None
+  action = time_data.get("action")
+  user_db = MongoHook(conn_id="mongo_user_db").get_conn().get_default_database()
+  test_user_id = int(Variable.get("test_user_id", '0'))
+  processing_batch_size = int(Variable.get("processing_batch_size", 100))
+  payload = {
+    "action": action,
+    "message": message,
+    "is_notification": False
+  }
+  user = user_db.get_collection("user")
+  user_filter = {
+    "userStatus": {"$in": [4]}
+  }
+  user_data = user.find(user_filter).batch_size(100)
+  http_hook = HttpHook(
+    method="POST",
+    http_conn_id="chat_service_url"
+  )
+  user_id_list = []
+  while user_data.alive:
+    for user in user_data:
+      patient_days = get_patient_days(patient=user)
+      if not patient_days:
+        continue
+      patient_days = patient_days - 1
+      if patient_days <= 6:
+        message = messages[patient_days]
+      if patient_days > 6:
+        if not len(dynamic_messages):
+          continue
+        message = dynamic_messages[0]
+      payload["message"] = message
+      user_id = user.get("userId")
+      if test_user_id and int(user_id) != test_user_id:
+        continue
+      try:
+        print(payload)
+        http_hook.run(endpoint="/api/v1/chat/user/" + str(round(user_id)) + "/message", data=json.dumps(payload))
+      except Exception as e:
+        raise ValueError(str(e))
+
+  # for i in range(0, len(user_id_list) + 1, processing_batch_size):
+  #   _id_list = user_id_list[i:i + processing_batch_size]
+  #   sleep(1)
+  #   for user_id in _id_list:
+  #     try:
+  #       http_hook.run(endpoint="/api/v1/chat/user/" + str(round(user_id)) + "/message", data=json.dumps(payload))
+  #     except Exception as e:
+  #       raise ValueError(str(e))
