84c9c2dc864434108ad7f72b6bc6976427fa15f9
Mrigesh Pokhrel <mrigeshpokhrel@gmail.com>
2019-03-28 19:15:26 +0530

    fix: reminder message type fix
    Signed-off-by: Mrigesh Pokhrel <mrigeshpokhrel@gmail.com>

--- reminders/reporting_reminder/dags.py
+++ reminders/reporting_reminder/dags.py
@@ -1,6 +1,7 @@
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from airflow.utils import dates
+from reminders.reporting_reminder.reminder import send_reminder
 
 from config import local_tz, default_args
 
@@ -15,7 +16,7 @@ reporting_reminder_19_00_dag = DAG(
 reporting_reminder_19_00_task = PythonOperator(
   task_id="reporting_reminder_19_00_task",
   task_concurrency=1,
-  python_callable=print,
+  python_callable=send_reminder,
   dag=reporting_reminder_19_00_dag,
   op_kwargs={},
   pool="task_reminder_pool",
