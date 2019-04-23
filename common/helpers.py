5bdd27df66d3fe9198c86550bab53b9530086966
Mrigesh Pokhrel <mrigeshpokhrel@gmail.com>
2019-04-23 20:07:39 +0530

    feat: fetch patients activated in last 10 minutes
    Signed-off-by: Mrigesh Pokhrel <mrigeshpokhrel@gmail.com>

---
+++ common/helpers.py
@@ -0,0 +1,33 @@
+from datetime import datetime, timedelta
+
+from airflow.contrib.hooks.mongo_hook import MongoHook
+
+from config import local_tz
+
+
+def create_health_plan():
+    pass
+
+
+def do_level_jump():
+    pass
+
+
+def get_activated_patients(**kwargs):
+    minutes_diff = kwargs.get("duration", 5)
+    to_time = datetime.now(tz=local_tz).replace(second=0, microsecond=0)
+    from_time = to_time - timedelta(minutes=minutes_diff)
+    user_db = MongoHook(conn_id="mongo_user_db").get_conn().get_default_database()
+    user = user_db.get_collection("user")
+    user_filter = {
+        "userFlags.active.activatedOn": {"$gt": to_time, "$lt": from_time},
+        "userStatus": 4,
+        "userFlags.active.activated": True
+    }
+    activated_patient_list = user.find(user_filter, projection={"patientId": 1, "_id": 0})
+    patient_id_list = []
+    for patient in activated_patient_list:
+        _id = patient.get("patientId")
+        if _id:
+            patient_id_list.append(_id)
+    return patient_id_list
