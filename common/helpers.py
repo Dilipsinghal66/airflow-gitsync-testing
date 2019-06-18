f646bf1bd1c4cec8022383625dff54d7fc6b35ad
Mrigesh Pokhrel <mrigeshpokhrel@gmail.com>
2019-06-18 15:39:54 +0530

    fix: get connection property :/ . Ref T163
    Signed-off-by: Mrigesh Pokhrel <mrigeshpokhrel@gmail.com>

--- common/helpers.py
+++ common/helpers.py
@@ -5,7 +5,7 @@ from airflow.contrib.hooks.redis_hook import RedisHook
 
 from config import local_tz
 
-redis_conn_callback = RedisHook(redis_conn_id="redis_callback").get_conn()
+redis_conn_callback = RedisHook(redis_conn_id="redis_callback").get_conn().client
 
 
 def create_health_plan():
