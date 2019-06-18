4d339d3c40a57fab0c167be1812a95974244cb10
Mrigesh Pokhrel <mrigeshpokhrel@gmail.com>
2019-06-18 14:30:33 +0530

    feat: change owner to twilio
    Signed-off-by: Mrigesh Pokhrel <mrigeshpokhrel@gmail.com>

--- config/__init__.py
+++ config/__init__.py
@@ -26,3 +26,13 @@ default_args = {
     'retries': 0,
     'retry_delay': timedelta(minutes=1)
 }
+
+twilio_args = {
+    'owner': 'twilio',
+    'depends_on_past': False,
+    'email': 'mrigesh@zyla.in',
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 0,
+    'retry_delay': timedelta(minutes=1)
+}
