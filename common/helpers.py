4fe16bceca8ea160a2a48ef0b87af5d5c35b4b6a
Mrigesh Pokhrel <mrigeshpokhrel@gmail.com>
2019-06-19 15:08:37 +0530

    feat: fetch message from redis and send. Ref T168
    Signed-off-by: Mrigesh Pokhrel <mrigeshpokhrel@gmail.com>

--- common/helpers.py
+++ common/helpers.py
@@ -2,12 +2,18 @@ from datetime import datetime, timedelta
 
 from airflow.contrib.hooks.mongo_hook import MongoHook
 from airflow.contrib.hooks.redis_hook import RedisHook
+from airflow.hooks.http_hook import HttpHook
+from airflow.hooks.base_hook import BaseHook
+from airflow.models import Connection
+from models.twilio import ChatService
 from redis import StrictRedis
-from common.statemachine import sendStateMachineMessage
+
 
 from config import local_tz
 
 redis_conn_callback: StrictRedis = RedisHook(redis_conn_id="redis_callback").get_conn()
+redis_conn_twilio_message: StrictRedis = RedisHook(redis_conn_id="redis_twilio_message").get_conn()
+twilio_cred_connections: Connection = BaseHook(source=None).get_connection(conn_id="twilio_credentials")
 
 
 def create_health_plan():
@@ -16,6 +22,24 @@ def create_health_plan():
 
 def do_level_jump():
     pass
+
+
+def get_user_by_filter(user_filter, projection=None, single=False):
+    user_db = MongoHook(conn_id="mongo_user_db").get_conn().get_default_database()
+    user_coll = user_db.get_collection("user")
+    if single:
+        user_data = user_coll.find_one(user_filter)
+    else:
+        user_data = user_coll.find(user_filter)
+    return user_data
+
+
+def update_user_activity(endpoint=None, payload=None):
+    extra_options = {
+        "check_response": True
+    }
+    activity_http_hook = HttpHook(method="PATCH", http_conn_id="http_services_url")
+    activity_http_hook.run(endpoint=endpoint, data=payload, extra_options=extra_options)
 
 
 def get_activated_patients(**kwargs):
@@ -39,7 +63,19 @@ def get_activated_patients(**kwargs):
 
 
 def send_twilio_message():
-    print("message sent")
+    extra_args = twilio_cred_connections.extra_dejson
+    chat_obj = ChatService(**extra_args)
+    process_num_messages = 10
+    twilio_message_redis_key = "twilio_message_list"
+    while process_num_messages:
+        if not redis_conn_twilio_message.exists(twilio_message_redis_key):
+            process_num_messages = 0
+            continue
+        if not redis_conn_twilio_message.llen(twilio_message_redis_key):
+            process_num_messages = 0
+            continue
+        send_message_data = redis_conn_twilio_message.lpop(twilio_message_redis_key)
+        chat_obj.send_message(send_message_data)
 
 
 def send_pending_callback_messages():
@@ -54,6 +90,7 @@ def send_pending_callback_messages():
         redis_conn_callback.delete(callback_key)
     callback_data = callback_cached_data.decode()
     try:
+        from common.statemachine import sendStateMachineMessage
         sendStateMachineMessage(callback_data)
     except Exception as e:
         redis_conn_callback.lpush(callback_key, callback_cached_data)
