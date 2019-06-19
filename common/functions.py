4705f7c0e2be7f722e0138a0c6a1193740e45d0a
Mrigesh Pokhrel <mrigeshpokhrel@gmail.com>
2019-06-20 01:59:54 +0530

    feat: python object generator from json
    Signed-off-by: Mrigesh Pokhrel <mrigeshpokhrel@gmail.com>

--- common/functions.py
+++ common/functions.py
@@ -1,8 +1,32 @@
 import json
-from types import SimpleNamespace
 
+class PyJSON(object):
+    def __init__(self, d):
+        if type(d) is str:
+            d = json.loads(d)
 
-def get_python_object(data_str: str):
-    obj_data = json.loads(data_str, object_hook=lambda d: SimpleNamespace(**d))
-    return obj_data
+        self.from_dict(d)
 
+    def from_dict(self, d):
+        self.__dict__ = {}
+        for key, value in d.items():
+            if type(value) is dict:
+                value = PyJSON(value)
+            self.__dict__[key] = value
+
+    def to_dict(self):
+        d = {}
+        for key, value in self.__dict__.items():
+            if type(value) is PyJSON:
+                value = value.to_dict()
+            d[key] = value
+        return d
+
+    def __repr__(self):
+        return str(self.to_dict())
+
+    def __setitem__(self, key, value):
+        self.__dict__[key] = value
+
+    def __getitem__(self, key):
+        return self.__dict__[key]
