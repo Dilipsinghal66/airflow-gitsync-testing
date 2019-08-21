c073857af2efba13fb4ef84f31ae8a78609aef22
Mrigesh Pokhrel <mrigeshpokhrel@gmail.com>
2019-08-21 14:01:23 +0530

    feat: debug log
    Signed-off-by: Mrigesh Pokhrel <mrigeshpokhrel@gmail.com>

--- tasks/dynamic_jobs.py
+++ tasks/dynamic_jobs.py
@@ -22,6 +22,7 @@ def get_cron_expression(job_timings=None):
     time_list = scheduled_time.split(":")
     try:
         date_list = scheduled_date.split("-")
+        print(date_list)
     except Exception as e:
         date_list = None
     cron_expression = time_list[1] + " " + time_list[0]
