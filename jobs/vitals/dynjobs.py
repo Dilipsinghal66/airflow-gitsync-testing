484a027034cde531684916f15deb6e89d2350e1c
Aishwary Bhashkar <aishwaray@gmail.com>
2019-08-25 20:06:56 +0530

    uncommented the code to run dyn jobs

--- jobs/vitals/dynjobs.py
+++ jobs/vitals/dynjobs.py
@@ -50,24 +50,24 @@ def send_dyn_func():
                 print(number_of_rows)
                 if number_of_rows != 0:
                     informationIdtobeSent = cursor.fetchone()[0]
-                    print(informationIdtobeSent)
+                    #print(informationIdtobeSent)
                     no_of_rows = cursor.execute("select distinct(id) from zylaapi.auth where phoneno in (select phoneno from zylaapi.patient_profile where id = " + str(key) + " )")
                     if no_of_rows > 0:
                         user_id = cursor.fetchone()[0]
 
-                        print(user_id)
+                        #print(user_id)
                         payload["message"] = str(informationIdtobeSent)
                         endpoint = "user/" + str(round(user_id)) + "/message"
                         #print(endpoint)
                         #print(payload)
-                        #status, body = make_http_request(
-                        #    conn_id="http_chat_service_url",
-                        #    endpoint=endpoint, method="POST", payload=payload)
+                        status, body = make_http_request(
+                            conn_id="http_chat_service_url",
+                            endpoint=endpoint, method="POST", payload=payload)
                         #print(status, body)
 
                         updateSqlQuery = "UPDATE zylaapi.patient_profile SET countDidYouKnow = " + str(informationIdtobeSent) + " where id = " + str(key)
                         #print(updateSqlQuery)
-                        #cursor.execute(updateSqlQuery)
+                        cursor.execute(updateSqlQuery)
 
                         sleep(.300)
 
@@ -83,4 +83,4 @@ def send_dyn_func():
         print(e)
 
 
-    #connection.commit()
+    connection.commit()
