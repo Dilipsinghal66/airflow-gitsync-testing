67b85d1d480284c67ebd501a722f297eb0344cf4
Mrigesh Pokhrel <mrigeshpokhrel@gmail.com>
2019-09-13 12:49:21 +0530

    feature: add feature to check slack integration for failed task testing
    Signed-off-by: Mrigesh Pokhrel <mrigeshpokhrel@gmail.com>

---
+++ tasks/fail_task_dag.py
@@ -0,0 +1,23 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+
+from config import local_tz, default_args
+
+failure_dag = DAG(
+    dag_id="TaskFailureTest",
+    default_args=default_args,
+    schedule_interval="@daily",
+    catchup=False,
+    start_date=datetime(year=2019, month=3, day=31, hour=0, minute=0, second=0,
+                        microsecond=0, tzinfo=local_tz),
+    dagrun_timeout=timedelta(minutes=1),
+)
+
+failure_task = BashOperator(
+    task_id="TaskFailureTestTask",
+    dag=failure_dag,
+    bash_command="$((1/0))",
+    task_concurrency=1
+)
