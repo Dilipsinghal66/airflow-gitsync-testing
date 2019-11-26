1394e8434bcc400fb06f485c7d7241979c25d998
Mrigesh Pokhrel <mrigeshpokhrel@gmail.com>
2019-11-26 14:36:49 +0530

    feat: continue state machine for user
    Signed-off-by: Mrigesh Pokhrel <mrigeshpokhrel@gmail.com>

--- common/helpers.py
+++ common/helpers.py
@@ -787,3 +787,19 @@ def refresh_daily_message():
         conn_id="http_statemachine_url", endpoint=dynamic_message_endpoint,
         method="GET")
     return dynamic_message_list
+
+
+def continue_statemachine():
+    redis_hook = RedisHook(redis_conn_id="redis_continue_statemachine")
+    redis_conn = redis_hook.get_conn()
+    redis_key = "chat::sm_continue"
+    while redis_conn.scard(redis_key):
+        user_id = redis_conn.srandmember(redis_key)
+        _filter = {
+            "userId": user_id
+        }
+        user_data = get_data_from_db(
+            conn_id="mongo_user_db",
+            **_filter
+        )
+        log.info(user_data)
---
+++ tasks/sm_continue_dag.py
@@ -0,0 +1,27 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+from common.helpers import add_sales_cm
+from config import local_tz, default_args
+
+sm_continue_dag = DAG(
+    dag_id="sm_continue",
+    default_args=default_args,
+    schedule_interval="@once",
+    catchup=False,
+    start_date=datetime(year=2019, month=3, day=31, hour=0, minute=0, second=0,
+                        microsecond=0, tzinfo=local_tz),
+    dagrun_timeout=timedelta(minutes=1),
+)
+
+add_sales_cm_task = PythonOperator(
+    task_id="continue_sm_task",
+    task_concurrency=1,
+    python_callable=add_sales_cm,
+    dag=sm_continue_dag,
+    op_kwargs={},
+    pool="scheduled_jobs_pool",
+    retry_exponential_backoff=True
+)
