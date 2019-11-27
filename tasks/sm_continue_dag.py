5e43f88f3e8229ee788bb937350e70d81a55fefa
Mrigesh Pokhrel <mrigeshpokhrel@gmail.com>
2019-11-27 17:11:44 +0530

    fix: remove sales uses done
    Signed-off-by: Mrigesh Pokhrel <mrigeshpokhrel@gmail.com>

--- tasks/sm_continue_dag.py
+++ tasks/sm_continue_dag.py
@@ -9,7 +9,7 @@ from config import local_tz, default_args
 sm_continue_dag = DAG(
     dag_id="sm_continue",
     default_args=default_args,
-    schedule_interval="@once",
+    schedule_interval="@every_5_minutes",
     catchup=False,
     start_date=datetime(year=2019, month=3, day=31, hour=0, minute=0, second=0,
                         microsecond=0, tzinfo=local_tz),
