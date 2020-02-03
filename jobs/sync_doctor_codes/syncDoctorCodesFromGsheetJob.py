7a9078da147dfd76c1ae4e8304c6e9b945ae177f
RajatGarg97 <rg51096@gmail.com>
2020-02-03 16:21:06 +0530

    Eliminated sql queries and added abstraction

--- jobs/sync_doctor_codes/syncDoctorCodesFromGsheetJob.py
+++ jobs/sync_doctor_codes/syncDoctorCodesFromGsheetJob.py
@@ -1,210 +1,74 @@
-from googleapiclient.discovery import build
-from airflow.contrib.hooks.gcp_api_base_hook import GoogleCloudBaseHook
-from airflow.models import Variable
 from common.db_functions import get_data_from_db
-from typing import Dict, Optional
 import pandas as pd
+from airflow.models import Variable
+from common.custom_hooks.google_sheets_hook import GSheetsHook
 
 
-SPREADSHEET_ID = str(Variable.get('SPREADSHEET_ID', '0'))
-RANGE_NAME = 'Sheet1'
-STATUS = 4
-TYPE = 0
-DESCRIPTION = "AZ"
-INITIATED_BY = "SCHEDULED TASK"
-TABLE_NAME = ""  # Mention table name along with db name
-
-
-######## GETTING DATA FROM GOOGLE SPREADSHEET ######
-
-# noinspection PyAbstractClass
-class GoogleSheetsHook(GoogleCloudBaseHook):
-    """
-    Hook for the Google Drive APIs.
-
-    :param api_version: API version used (for example v3).
-    :type api_version: str
-    :param gcp_conn_id: The connection ID to use when fetching connection info.
-    :type gcp_conn_id: str
-    :param delegate_to: The account to impersonate, if any.
-        For this to work, the service account making the request must have
-        domain-wide delegation enabled.
-    :type delegate_to: str
+def dump_data_in_db(table_name, spreadsheet_data, engine):
     """
 
-    _conn = None
+    :param table_name: Name of the table where data is to be written
+    :param spreadsheet_data: Data from the GSheetsHook
+    :param engine: MySqlHook object from common.db_functions
+    :return: Nothing
+    """
 
-    def __init__(
-            self,
-            spreadsheet_id: str,
-            gcp_conn_id: str = 'gcp_sheet',
-            api_version: str = 'v4',
-            delegate_to: Optional[str] = None
-    ) -> None:
-        super().__init__(gcp_conn_id, delegate_to)
-        self.spreadsheet_id = spreadsheet_id
-        self.gcp_conn_id = gcp_conn_id
-        self.api_version = api_version
-        self.delegate_to = delegate_to
-        self._conn = None
+    spreadsheet_data['description'] = "AZ"
+    spreadsheet_data['status'] = 4
+    spreadsheet_data['type'] = 0
+    spreadsheet_data['initiated_by'] = "SCHEDULED TASK"
+    spreadsheet_data['licenseNumber'] = spreadsheet_data['Doctor Code']
 
-    def get_conn(self):
-
-        """
-        Retrieves the connection to Google Drive.
-
-
-        :return: Google Drive services object.
-        """
-        if not self._conn:
-            http_authorized = self._authorize()
-            self._conn = build('sheets', self.api_version, http=http_authorized, cache_discovery=False)
-        return self._conn
-
-    @GoogleCloudBaseHook.catch_http_exception
-    def get_values(
-            self,
-            range_: str,
-            major_dimension: str,
-            value_render_option: str = 'FORMATTED_VALUE',
-            date_time_render_option: str = 'SERIAL_NUMBER'
-    ) -> Dict:
-        """
-        Gets values from Google Sheet from a single range
-        https://developers.google.com/sheets/api/reference/rest/v4/spreadsheets.values/get
-        :param range_: The A1 notation of the values to retrieve.
-        :type range_: str
-        :param major_dimension: Indicates which dimension an operation should apply to.
-            DIMENSION_UNSPECIFIED, ROWS, or COLUMNS
-        :type major_dimension: str
-        :param value_render_option: Determines how values should be rendered in the output.
-            FORMATTED_VALUE, UNFORMATTED_VALUE, or FORMULA
-        :type value_render_option: str
-        :param date_time_render_option: Determines how dates should be rendered in the output.
-            SERIAL_NUMBER or FORMATTED_STRING
-        :type date_time_render_option: str
-        :return: Google Sheets API response.
-        :rtype: List
-        """
-        service = self.get_conn()
-        response = service.spreadsheets().values().get(  # pylint: disable=no-member
-            spreadsheetId=self.spreadsheet_id,
-            range=range_,
-            majorDimension=major_dimension,
-            valueRenderOption=value_render_option,
-            dateTimeRenderOption=date_time_render_option
-        ).execute()
-
-        return response.get('values', [])
-
-
-######## DATABASE SYNC CODE #########
-
-# check_for_null() checks the data for null values and substitutes it with an empty string
-def check_for_null(input_string):
-
-    if input_string is not None:
-        return input_string
-    else:
-        return " "
-
-
-# record_exist() checks the database for existence of the record.
-# It helps in deciding if the query should be INSERT or UPDATE.
-def record_exist(spreadsheet_data, row, cursor):
-
-    select_query = "SELECT EXISTS(SELECT * FROM " + TABLE_NAME + " WHERE phoneno = '" + \
-                   str(spreadsheet_data['Phone Number (+91)'][row]) + "');"
-
-    cursor.execute(select_query)
-    response = cursor.fetchone()[0]
-
-    if response == '0':
-        return False
-    else:
-        return True
-
-
-# dump_data_in_db handles updating of data in the mysql database
-def dump_data_in_db(spreadsheet_data, cursor, connection):
-
-    # print("\n ##### In function dump data ##### \n")
+    row_list = [[]]
 
     for row in range(len(spreadsheet_data)):
 
         if spreadsheet_data['Phone Number (+91)'][row] is not None:
 
-            if record_exist(spreadsheet_data, row, cursor):
-                print("\n Update sql query")
-                update_sql_query = " UPDATE " + TABLE_NAME + " SET " \
-                                   "profile_image = '" + \
-                                   str(check_for_null(spreadsheet_data['Photo (Keep name of photo as doctor code)'][row])) + "', " +\
-                                   "name = '" + str(spreadsheet_data['Name of Dcotor'][row]) + "', " + \
-                                   "speciality = '" + str(check_for_null(spreadsheet_data['Speciality'][row])) + "', " \
-                                   + "clinicHospital = '" + str(check_for_null(spreadsheet_data['Hospital/Clinic'][row])) \
-                                   + "', " + "location = '" + str(check_for_null(spreadsheet_data['City'][row])) + "', " \
-                                   + "licenseNumber = '" + str(spreadsheet_data['Doctor Code'][row]) + "', " + \
-                                   "status = '" + str(STATUS) + "', " + \
-                                   "initiated_by = '" + str(INITIATED_BY) + "', " + \
-                                   "type = '" + str(TYPE) + "', " + \
-                                   "description = '" + str(DESCRIPTION) + "', " + \
-                                   "title = '" + str(spreadsheet_data['Title'][row]) + "', " + \
-                                   "code = '" + str(spreadsheet_data['Doctor Code'][row]) + "', " + \
-                                   "email = '" + str(check_for_null(spreadsheet_data['Email Id'][row])) + \
-                                   "' WHERE phoneno = '" + str(spreadsheet_data['Phone Number (+91)'][row]) + "'"
-                print(update_sql_query)
-                cursor.execute(update_sql_query)
+            row_list.append(spreadsheet_data[row])
 
-            else:
-                print("\n Insert sql query")
-                insert_sql_query = "INSERT INTO " + TABLE_NAME + " (phoneno, profile_image, name, " \
-                               "speciality, clinicHospital, location, licenseNumber, status, " \
-                               "initiated_by, type, description, title, code, email) VALUES ('" + \
-                               str(spreadsheet_data['Phone Number (+91)'][row]) + "', '" + \
-                               str(check_for_null(spreadsheet_data['Photo (Keep name of photo as doctor code)'][row])) \
-                               + "', '" + \
-                               str(spreadsheet_data['Name of Dcotor'][row]) + "', '" + \
-                               str(check_for_null(spreadsheet_data['Speciality'][row])) + "', '" + \
-                               str(check_for_null(spreadsheet_data['Hospital/Clinic'][row])) + "', '" + \
-                               str(check_for_null(spreadsheet_data['City'][row])) + "', '" + \
-                               str(spreadsheet_data['Doctor Code'][row]) + "', '" + \
-                               str(STATUS) + "', '" + \
-                               str(INITIATED_BY) + "', '" + \
-                               str(TYPE) + "', '" + \
-                               str(DESCRIPTION) + "', '" + \
-                               str(spreadsheet_data['Title'][row]) + "', '" + \
-                               str(spreadsheet_data['Doctor Code'][row]) + "', '" + \
-                               str(check_for_null(spreadsheet_data['Email Id'][row])) + "');"
+    engine.insert_rows(table_name, row_list,
+                       target_fields='code, name, title, phoneno, email, '
+                                     'speciality, clinicHospital, location, '
+                                     'profile_image, description, status, '
+                                     'type, initiated_by, licenseNumber',
+                       commit_every=100, replace=True)
 
-                print(insert_sql_query)
-                cursor.execute(insert_sql_query)
-
-    connection.commit()
-    print("Successfully Committed")
+#    print("Successfully Committed")
 
 
-# initializer() is the driver function for this script
 def initializer():
+    # initializer() is the driver function for this script
 
-    if SPREADSHEET_ID == '0':
-        return "Invalid Spreadsheet Id"
+    spreadsheet_id_conn_id = str(Variable.get('spreadsheet_id_conn_id', '0'))
+    spreadsheet_id_conn_id.split('|')
+    spreadsheet_id = spreadsheet_id_conn_id[0]
+    gcp_conn_id = spreadsheet_id_conn_id[1]
 
-    g_sheet_hook = GoogleSheetsHook(SPREADSHEET_ID)
+    if spreadsheet_id == '0':
+        return
 
-    g_sheet_hook.get_conn()
+    table_name = ""  # Mention table name along with db name
 
-    spreadsheet_data = g_sheet_hook.get_values(range_=RANGE_NAME, major_dimension="ROWS")
+    range_names = ['Sheet1!A:D', 'Sheet1!F:J']
 
-    spreadsheet_data = pd.DataFrame(spreadsheet_data[1:], columns=spreadsheet_data[0])
+    sheet_hook = GSheetsHook(
+        spreadsheet_id=spreadsheet_id,
+        gcp_conn_id=gcp_conn_id,
+        api_version="v4"
+    )
+
+    sheet_conn = sheet_hook.get_conn()
+
+    spreadsheet_data = sheet_conn.batch_get_values(ranges=range_names,
+                                                   major_dimension='ROWS').\
+        get('values')
+
+    spreadsheet_data = pd.DataFrame(spreadsheet_data[1:],
+                                    columns=spreadsheet_data[0])
 
     engine = get_data_from_db(db_type="mysql", conn_id="mysql_monolith")
-    connection = engine.get_conn()
-    cursor = connection.cursor()
 
-    dump_data_in_db(spreadsheet_data, cursor, connection)
-
-
-
-
+    dump_data_in_db(table_name, spreadsheet_data, engine)
 
 
