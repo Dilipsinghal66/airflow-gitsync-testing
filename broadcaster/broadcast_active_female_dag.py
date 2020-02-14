223d4a47301c58a78908cca3f731c9888169b37c
RajatGarg97 <rg51096@gmail.com>
2020-02-14 12:33:46 +0530

    feat: added active female broadcast and dag files. inprogress T603

---
+++ broadcaster/broadcast_active_female_dag.py
@@ -0,0 +1,24 @@
+from datetime import datetime, timedelta
+from broadcaster.broadcast_active_female import broadcast_active_fm
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from config import default_args, local_tz
+
+broadcast_active_female_dag = DAG(
+    dag_id="broadcast_active_female_dag",
+    default_args=default_args,
+    start_date=datetime(year=2020, month=2, day=3, hour=9, minute=0, second=0,
+                        microsecond=0, tzinfo=local_tz),
+    schedule_interval=timedelta(days=0, seconds=0, microseconds=0,
+                                milliseconds=0, minutes=0, hours=12, weeks=0),
+    catchup=False
+)
+
+broadcast_active_female_task = PythonOperator(
+    task_id="broadcast_active_female_task",
+    task_concurrency=1,
+    python_callable=broadcast_active_fm,
+    dag=broadcast_active_female_dag,
+    op_kwargs={},
+    pool="scheduled_jobs_pool",
+)
