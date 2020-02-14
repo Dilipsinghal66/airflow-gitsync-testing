f856685230fa735141a607ee1e96c74d0f09b052
RajatGarg97 <rg51096@gmail.com>
2020-02-14 11:31:39 +0530

    fix: removed repeated calls and added parameters. inprogress T603

--- broadcaster/broadcast_queries.py
+++ broadcaster/broadcast_queries.py
@@ -1,7 +1,7 @@
-# import json
+import json
 from airflow.models import Variable
 from airflow.utils.log.logging_mixin import LoggingMixin
-# from common.pyjson import PyJSON
+from common.pyjson import PyJSON
 import datetime
 from common.db_functions import get_data_from_db
 
@@ -23,18 +23,40 @@ def broadcast_queries():
     :return:
     """
 
+    config_var = Variable.get('broadcast_query_config', None)
+
+    if config_var:
+        config_var = json.loads(config_var)
+        config_obj = PyJSON(d=config_var)
+    else:
+        raise ValueError("Config variables not defined")
+
+
+    try:
+        table1 = config_obj.gcp
+        sheet = config_obj.sheet
+        db = config_obj.db
+        validation_schema = config_obj.validation
+        defaults = config_obj.defaults
+
+    except Exception as e:
+        warning_message = "Couldn't get config variables"
+        log.warning(warning_message)
+        log.error(e, exc_info=True)
+        raise e
+
     sql_query_male = str(Variable.get("paid_male_patients",
-                                      'SELECT id FROM '
+                                      'SELECT %s FROM '
                                       'zylaapi.patient_profile '
                                       'WHERE status = 4 AND gender = 2'))
 
     sql_query_female = str(Variable.get("paid_female_patients",
-                                        'SELECT id FROM '
+                                        'SELECT %s FROM '
                                         'zylaapi.patient_profile '
                                         'WHERE status = 4 AND gender = 1'))
 
     sql_query_meditation = str(Variable.get("no_meditation",
-                                            'SELECT id FROM '
+                                            'SELECT %s FROM '
                                             'zylaapi.patient_profile '
                                             'WHERE id NOT IN '
                                             '(SELECT DISTINCT '
@@ -64,14 +86,17 @@ def broadcast_queries():
         log.debug(sql_query_female)
         log.debug(sql_query_meditation)
 
-        data1 = engine.get_records(sql=sql_query_male,
-                                   parameters=None)
-        log.debug(data1)
+        data = engine.get_records(sql=[sql_query_male,
+                                       sql_query_female,
+                                       sql_query_meditation],
+                                  parameters=['id', 'id', 'id'])
+        log.debug(data)
 
         # data2 = engine.get_records(sql=sql_query_female, parameters='id')
 
-        data3 = engine.get_records(sql=sql_query_meditation, parameters=['id'])
-        log.debug(data3)
+        # data3 = engine.get_records(sql=sql_query_meditation,
+        #                            parameters=['id'])
+        # log.debug(data3)
 
     except Exception as e:
         warning_message = "Query on mysql database unsuccessful"
