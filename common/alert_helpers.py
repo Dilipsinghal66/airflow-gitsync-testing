39de111eb1c7afc4a888f9b0422313b477b661a1
RajatGarg97 <rg51096@gmail.com>
2020-02-17 18:04:07 +0530

    feat: added fail alert email method. inprogress T821

--- common/alert_helpers.py
+++ common/alert_helpers.py
@@ -1,4 +1,5 @@
 from airflow.contrib.hooks.slack_webhook_hook import SlackWebhookHook
+from airflow.operators.email_operator import EmailOperator
 
 
 def task_failure_slack_alert(context):
@@ -24,3 +25,34 @@ def task_failure_slack_alert(context):
 
 def task_success_slack_alert(context):
     pass
+
+
+def task_failure_email_alert(context):
+
+    ti = context.get('task_instance')
+
+    email_msg = """
+                Task: {task}
+                Dag: {dag}
+                Execution Time: {exec_date}
+                Log Url: {log_url}
+                Here is the list of Doctor Codes from AZ doctor spreadsheet
+                which have failed schema validation.
+                """.format(task=ti.task_id,
+                           dag=ti.dag_id,
+                           exec_date=context.get('execution_date'),
+                           log_url=ti.log_url
+                           )
+
+    subject_msg = "List of Doctors failing validation in AZ sync"
+    failed_records = ti.xcom_pull()
+
+    email_msg.__add__(str(failed_records))
+
+    email_obj = EmailOperator(to='rajat@zyla.in',
+                              subject=subject_msg,
+                              html_content=email_msg,
+                              task_id=ti.task_id
+                              )
+
+    return email_obj.execute(context=context)
