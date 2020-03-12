5c03a28fd5a4bf12bd1edfb44841ff6f92aca78e
RajatGarg97 <rg51096@gmail.com>
2020-03-12 18:55:38 +0530

    fix: changed scheduled time. inprogress T954

--- jobs/sync_doctor_codes/sync_doctor_codes_analytics.py
+++ jobs/sync_doctor_codes/sync_doctor_codes_analytics.py
@@ -4,13 +4,25 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from config import default_args, local_tz
 from common.alert_helpers import task_failure_email_alert
+from airflow.models import Variable
+from common.pyjson import PyJSON
+import json
+
+config_var = Variable.get('doctor_sync_analytics_config', None)
+
+if config_var:
+    config_var = json.loads(config_var)
+    config_obj = PyJSON(d=config_var)
+    cron_time = config_obj.time.cron
+else:
+    raise ValueError("Config variables not defined")
 
 sync_doctor_codes_analytics_dag = DAG(
     dag_id="sync_doctor_codes_analytics_dag",
     default_args=default_args,
     start_date=datetime(year=2020, month=2, day=3, hour=9, minute=0, second=0,
                         microsecond=0, tzinfo=local_tz),
-    schedule_interval="0 9 * * *",
+    schedule_interval=cron_time,
     catchup=False
 )
 
