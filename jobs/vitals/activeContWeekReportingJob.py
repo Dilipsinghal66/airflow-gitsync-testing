fb6ac68bc6d091ecf20a449e77713eab9f9d39e9
RajatGarg97 <rg51096@gmail.com>
2020-03-23 17:04:47 +0530

    fix: fixed creation of csv file and mailing method. inprogress T980

--- jobs/vitals/activeContWeekReportingJob.py
+++ jobs/vitals/activeContWeekReportingJob.py
@@ -11,52 +11,53 @@ from common.pyjson import PyJSON
 log = LoggingMixin().log
 
 
-def email_notifier(alerts_default, active_patientid_list):
+def email_notifier(dest_email, start_date, active_patientid_list, context):
 
-    email_msg = """
-                    <b> Pl. find the list of patients actively reporting <br> 
-                    in the duration ({fromDate} to {toDate}) attached with 
-                    this mail. 
-                    </b> 
-                    """.format(fromDate=str(alerts_default.start_date.date()),
-                               toDate=str(date.today()))
+    email_msg = """ Pl. find the list of patients actively reporting <br>
+                    in the duration <b>({fromDate} to {toDate})</b> attached
+                    with this mail. <br>""".format(
+        fromDate=str(start_date.date()),
+        toDate=str(date.today()))
 
     subject_msg = "List of active patients continuously reporting for a week"
     files = []
 
     try:
 
-        if active_patientid_list is not None:
+        if active_patientid_list.__len__():
 
             filename = 'continuousReporting.csv'
 
-            with open(filename, 'w', newline=" ") as csvfile:
+            with open(filename, 'w') as csvfile:
                 writer = csv.writer(csvfile)
-                writer.writerow('PatientId')
+                writer.writerow(['PatientId'])
 
-                for pid in active_patientid_list:
-                    writer.writerow(pid)
+                for pid in range(len(active_patientid_list)):
+                    writer.writerow([active_patientid_list[pid]])
 
             files.append(filename)
 
         else:
-            email_msg = email_msg + "<br><b> No patients were found " \
+            email_msg = email_msg + "<br><b>NOTE: No patients were found " \
                                     "reporting continuously for the " \
                                     "given duration </b>"
 
     except Exception as e:
-        warning_message = "Could't make list of patients"
+        warning_message = "Could't make csv file"
         log.warning(warning_message)
         log.error(e, exc_info=True)
         raise e
 
     try:
 
-        email_obj = EmailOperator(to=alerts_email,
+        ti = context.get('task_instance')
+
+        email_obj = EmailOperator(to=dest_email,
                                   subject=subject_msg,
                                   html_content=email_msg,
                                   files=files,
-                                  mime_charset='utf-8'
+                                  mime_charset='utf-8',
+                                  task_id=ti.task_id
                                   )
     except Exception as e:
         warning_message = "Email operator could not be instantiated"
@@ -64,19 +65,28 @@ def email_notifier(alerts_default, active_patientid_list):
         log.error(e, exc_info=True)
         raise e
 
-    return email_obj.execute(context=None)
+    return email_obj.execute(context=context)
 
 
-def active_week_reporting(**kwargs):
+def active_week_reporting(**context):
 
-    process_cont_week_active_reporting = int(Variable.get("process_cont_week_"
-                                                          "active_reporting_"
-                                                          "disable", "0"))
-    if process_cont_week_active_reporting:
-        return
+    config_var = Variable.get('cont_weekly_reporting_config', None)
 
-    delta = 7
-    start_date = datetime.now() - timedelta(days=delta)
+    if config_var:
+        config_var = json.loads(config_var)
+        config_obj = PyJSON(d=config_var)
+        if config_obj.disable:
+            return
+        status_db = config_obj.status_db
+        reporting_db = config_obj.reporting_db
+        defaults = config_obj.defaults
+        interval = defaults.interval
+        dest_email = defaults.email
+
+    else:
+        raise ValueError("Config variables not defined")
+
+    start_date = datetime.now() - timedelta(days=interval)
 
     user_filter = {
         "userStatus": {"$in": [4]}
@@ -90,13 +100,14 @@ def active_week_reporting(**kwargs):
 
     try:
 
-        active_userid_cursor = get_data_from_db(conn_id='mongo_user_db',
-                                                collection="user",
-                                                filter=user_filter,
-                                                projection=user_projection)
+        active_userid_cursor = get_data_from_db(
+            conn_id=status_db.conn_id,
+            collection=status_db.collection_name,
+            filter=user_filter,
+            projection=user_projection)
 
         active_userid = []
-        active_patientid = [1703]
+        active_patientid = []
 
         for user in active_userid_cursor:
             user_id = user.get("userId")
@@ -129,16 +140,17 @@ def active_week_reporting(**kwargs):
                         "count": {"$sum": 1}
                         }
              },
-            {"$match": {"count": {"$gte": delta},
+            {"$match": {"count": {"$gte": interval},
                         "_id": {"$in": active_patientid}
                         }
              }
                     ]
 
-        mongo_hook = MongoHook(conn_id='mongo_reporting_db')
+        mongo_hook = MongoHook(conn_id=reporting_db.conn_id)
 
-        response = mongo_hook.aggregate(mongo_collection='report_date',
-                                        aggregate_query=pipeline)
+        response = mongo_hook.aggregate(
+            mongo_collection=reporting_db.collection_name,
+            aggregate_query=pipeline)
 
         cont_active_patientid_list = []
 
@@ -149,11 +161,13 @@ def active_week_reporting(**kwargs):
             log.debug("Patient ID received " + str(patient_id) + ", count " +
                       str(count))
 
-        email_notifier(alerts_default=alerts_default,
-                       active_patientid_list=cont_active_patientid_list)
-
     except Exception as e:
         warning_message = "Second Query on MongoDB unsuccessful"
         log.warning(warning_message)
         log.error(e, exc_info=True)
         raise e
+
+    email_notifier(dest_email=dest_email,
+                   start_date=start_date,
+                   active_patientid_list=cont_active_patientid_list,
+                   context=context)
