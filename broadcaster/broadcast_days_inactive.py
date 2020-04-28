f9eccc3435010523d4f0cd25707f7accdd2c46dc
Aishwary Bhashkar <aishwaray@gmail.com>
2020-04-28 18:20:24 +0530

    feat: boradcast query for inactive for more than 2 days T1102

---
+++ broadcaster/broadcast_days_inactive.py
@@ -0,0 +1,31 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.models import Variable
+from airflow.operators.python_operator import PythonOperator
+
+from config import local_tz, default_args
+from broadcaster.broadcast_days_inactive_jobs import broadcast_days_inactive
+
+broadcast_days_inactive_cron = str(Variable.get
+                                 ("broadcast_days_inactive_cron", '@yearly'))
+
+broadcast_days_inactive_dag = DAG(
+    dag_id="broadcast_days_inactive",
+    default_args=default_args,
+    schedule_interval=broadcast_days_inactive_cron,
+    catchup=False,
+    start_date=datetime(year=2019, month=3, day=31, hour=0, minute=0, second=0,
+                        microsecond=0, tzinfo=local_tz),
+    dagrun_timeout=timedelta(minutes=1),
+)
+
+broadcast_days_inactive_task = PythonOperator(
+    task_id="broadcast_days_inactive",
+    task_concurrency=1,
+    python_callable=broadcast_days_inactive,
+    dag=broadcast_days_inactive_dag,
+    op_kwargs={},
+    pool="scheduled_jobs_pool",
+    retry_exponential_backoff=True
+)
---
+++ broadcaster/broadcast_days_inactive_jobs.py
@@ -0,0 +1,54 @@
+from airflow.models import Variable
+from airflow.utils.log.logging_mixin import LoggingMixin
+import datetime
+
+from common.db_functions import get_data_from_db
+from common.helpers import process_dynamic_message
+
+log = LoggingMixin().log
+
+
+def broadcast_days_inactive():
+
+    process_broadcast_days_inactive_disable = int(Variable.get
+                                                ("process_broadcast_days_"
+                                                 "inactive_disable", '0'))
+    if process_broadcast_days_inactive_disable == 1:
+        return
+
+    message = str(Variable.get("broadcast_days_inactive_msg", ''))
+
+    delta = int(Variable.get("broadcast_days_inactive_delta", '2'))
+    start_date = datetime.datetime.today() - datetime.timedelta(days=delta)
+    _filter = {
+        "lastActivity": {"$lte": start_date}
+    }
+
+    projection = {
+        "_id": 1
+    }
+    userid_cursor = get_data_from_db(conn_id="mongo_user_db",
+                                     collection="user_activity",
+                                     filter=_filter,
+                                     projection=projection)
+    mongo_filter_field = "_id"
+
+    userid_list = []
+    for user in userid_cursor:
+        user_id = user.get("_id")
+        userid_list.append(user_id)
+        log.info("Object ID received " + str(user_id))
+
+    _filter = {
+        mongo_filter_field: {"$in": userid_list},
+        "countryCode": {"$in": [91]},
+        "docCode": {"$regex": "^ZH"},
+        "userFlags.active.activated": {"$eq": True}
+    }
+    projection = {
+        "userId": 1, "_id": 0
+    }
+    message_replace_data = {}
+    action = "dynamic_message"
+    process_dynamic_message(_filter, projection,
+                            message_replace_data, message, action)
