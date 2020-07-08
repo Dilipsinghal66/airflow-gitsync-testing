376d4388ead8bdda39decb9d8ebe1fcb1316f9c8
Aishwary Bhashkar <aishwaray@gmail.com>
2020-07-08 14:52:00 +0530

    feat: adding headers

--- common/http_functions.py
+++ common/http_functions.py
@@ -14,7 +14,7 @@ def make_http_request(conn_id=None, method=None, payload=None, endpoint=""):
     hook_obj = HttpHook(method=method, http_conn_id=conn_id)
     if method in payload_required_methods:
         payload = json.dumps(payload)
-        response = hook_obj.run(endpoint=endpoint, data=payload, headers=extra_http_options,
+        response = hook_obj.run(endpoint=endpoint, data=payload, headers=str(extra_http_options),
                                 extra_options=extra_http_options)
     elif method in disable_response_check_methods:
         response = hook_obj.run(endpoint=endpoint)
