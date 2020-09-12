1a51a6bbb431c0d96040d44e3095e9e647402f90
Pranjal <pranjal9599@gmail.com>
2020-09-12 20:56:35 +0530

    fix date

--- jobs/patient_journey/patientJourneyJob.py
+++ jobs/patient_journey/patientJourneyJob.py
@@ -19,8 +19,9 @@ def getPatientStatus():
 
         cursor.execute("select patient_id,from_status,to_status,updated_on from zylaapi.patient_status_audit")
         patientIds = {}
+        currentDate = datetime.now()
         for row in cursor.fetchall():
-            patientIds[row[0]] = row[2]
+            patientIds[row[0]] = currentDate - row[3]
             log.info(row)
         
         return patientIds
@@ -59,5 +60,5 @@ def initializer(**kwargs):
             "message": messages[patients[p]],
             "is_notification": False
         }
-        send_chat_message_patient_id(patient_id=int(p), payload=payload)
+        #send_chat_message_patient_id(patient_id=int(p), payload=payload)
         log.info("Sending {} day {} message {}".format(p, patients[p], messages[patients[p]]))
