a4c64b3f9106e6113cdf79a3347a6a33aac2e599
Pranjal <pranjal9599@gmail.com>
2020-09-22 13:07:33 +0530

    patient journey bug fix

--- jobs/patient_journey/patientJourneyJob.py
+++ jobs/patient_journey/patientJourneyJob.py
@@ -11,31 +11,35 @@ from datetime import datetime
 log = LoggingMixin().log
 
 
-
 def getPatientStatus():
-    try:        
+    try:
         engine = get_data_from_db(db_type="mysql", conn_id="mysql_monolith")
         connection = engine.get_conn()
         cursor = connection.cursor()
 
-        cursor.execute("select patient_id,from_status,to_status,updated_on from zylaapi.patient_status_audit where to_status=11")
+        cursor.execute(
+            "select patient_id,from_status,to_status,updated_on from zylaapi.patient_status_audit")
         patientIds = {}
+        patientToStatues = {}
         currentDate = datetime.now().date()
         for row in cursor.fetchall():
             datedel = currentDate - row[3]
             patientIds[row[0]] = datedel.days
+            patientToStatues[row[0]] = row[2]
             log.info(row)
-        
-        return patientIds
+
+        return patientIds, patientToStatues
 
     except Exception as e:
         log.info("Error Exception raised")
         log.info(e)
 
+
 def getJourneyMessages(time):
     try:
         mongo_conn = MongoHook(conn_id="mongo_prod").get_conn()
-        collection = mongo_conn.get_database("trialMessageJourney").get_collection("messages")
+        collection = mongo_conn.get_database(
+            "trialMessageJourney").get_collection("messages")
         results = collection.find({'Time': time})
         messages = {}
 
@@ -43,30 +47,32 @@ def getJourneyMessages(time):
         for q in results:
             messages[q['Day']] = q['Message']
             log.info(q)
-        
+
         return messages
     except Exception as e:
         log.info("Error Exception raised")
-        log.info(e) 
+        log.info(e)
+
 
 def initializer(**kwargs):
     time = kwargs['time']
     log.info(time)
     log.info("Starting...")
-    patients = getPatientStatus()
+    patients, patientStatuses = getPatientStatus()
     messages = getJourneyMessages(time)
+    print(patientStatuses, patients)
 
     for p in patients:
-        if patients[p] > 0:
+        if patients[p] > 0 and patientStatuses[p] == 11:
             payload = {
                 "action": "dynamic_message",
                 "message": messages[patients[p]],
                 "is_notification": False
             }
             try:
-                send_chat_message_patient_id(patient_id=int(p), payload=payload)
-                log.info("Sending {} day {} message {}".format(p, patients[p], messages[patients[p]]))
+                #send_chat_message_patient_id(patient_id=int(p), payload=payload)
+                log.info("Sending {} day {} message {}".format(
+                    p, patients[p], messages[patients[p]]))
             except Exception as e:
                 log.info("Error Exception raised")
-                log.info(e) 
-
+                log.info(e)
