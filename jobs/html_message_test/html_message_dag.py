0dfa1b3ccbee3cec628d9acdeaaf169c5e7066fe
Pranjal <pranjal9599@gmail.com>
2020-09-23 16:38:33 +0530

    test html message

---
+++ jobs/html_message_test/html_message_dag.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from jobs.html_message_test.html_message_test_job import send_test_message
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from config import default_args, local_tz
+from common.alert_helpers import task_failure_email_alert
+from common.pyjson import PyJSON
+from airflow.models import Variable
+import json
+
+
+html_test_message_dag = DAG(
+    dag_id="html_test_message_dag",
+    start_date=datetime(year=2020, month=2, day=3, hour=9, minute=0, second=0,
+                        microsecond=0, tzinfo=local_tz),
+    schedule_interval="0 0 * * *",
+    catchup=False
+)
+
+bridge_automation_task = PythonOperator(
+    task_id="html_test_message_dag",
+    task_concurrency=1,
+    python_callable=send_test_message,
+    dag=html_test_message_dag,
+    op_kwargs={},
+    pool="scheduled_jobs_pool",
+    retry_exponential_backoff=True,
+    provide_context=True
+)
---
+++ jobs/html_message_test/html_message_test_job.py
@@ -0,0 +1,31 @@
+from common.helpers import send_chat_message_patient_id
+from common.pyjson import PyJSON
+from airflow.models import Variable
+import json
+from airflow.utils.log.logging_mixin import LoggingMixin
+
+
+log = LoggingMixin().log
+
+
+def send_test_message(**kargs):
+    config_var = Variable.get('html_message_config', None)
+
+    if config_var:
+        config_var = json.loads(config_var)
+        config_obj = PyJSON(d=config_var)
+    else:
+        raise ValueError("Config variables not defined")
+
+    payload = {
+        "action": "dynamic_message",
+        "message": str(config_obj.message),
+        "is_notification": False
+    }
+
+    try:
+        send_chat_message_patient_id(
+            patient_id=int(config_obj.patient_id), payload=payload)
+    except Exception as e:
+        log.info("Error Exception raised")
+        log.info(e)
