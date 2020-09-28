a9932cfc27309145aad599d5541b0fc0fe239641
Aishwary Bhashkar <aishwaray@gmail.com>
2020-09-28 13:31:17 +0530

    added code for monday and thrusday

--- jobs/daily_message_jobs.py
+++ jobs/daily_message_jobs.py
@@ -2,6 +2,7 @@ from airflow.models import Variable
 from common.db_functions import get_data_from_db
 from airflow.utils.log.logging_mixin import LoggingMixin
 from common.helpers import patient_id_message_send
+import datetime
 
 log = LoggingMixin().log
 
@@ -11,6 +12,15 @@ def daily_message():
     process_broadcast_active = int(Variable.get("process_daily_message",
                                                 '0'))
     if process_broadcast_active == 1:
+        return
+
+    date = datetime.datetime.today()
+    timedelta = datetime.timedelta(hours=5, minutes=30)
+    today_date = date + timedelta
+
+    day = today_date.weekday()
+
+    if (day == 0) or (day == 3):
         return
 
     engine = get_data_from_db(db_type="mysql", conn_id="mysql_monolith")
