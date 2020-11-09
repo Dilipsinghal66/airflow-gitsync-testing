3b6aa4fc4be0409365308baffe42a9ca0731e36e
Pranjal <pranjal9599@gmail.com>
2020-11-09 22:47:56 +0530

    fix no az active job

--- broadcaster/broadcast_active_no_az_jobs.py
+++ broadcaster/broadcast_active_no_az_jobs.py
@@ -1,6 +1,6 @@
 from airflow.models import Variable
 
-from common.helpers import process_dynamic_task_sql_no_az
+from common.helpers import process_dynamic_task_sql
 
 
 def broadcast_active_no_az():
@@ -13,8 +13,8 @@ def broadcast_active_no_az():
     sql_query = str(Variable.get("broadcast_active_sql_query",
                                  'select id from '
                                  'zylaapi.patient_profile '
-                                 'where status = 4 AND new_chat=1'))
+                                 'where status = 4 AND new_chat=1 and referred_by=0'))
 
     message = str(Variable.get("broadcast_active_no_az_msg", ''))
     action = "dynamic_message"
-    process_dynamic_task_sql_no_az(sql_query, message, action)
+    process_dynamic_task_sql(sql_query, message, action)
