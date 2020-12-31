afda2e3e8f6bcd3fc20b8e77b303ed52fc0eef0c
Pranjal <pranjal9599@gmail.com>
2020-12-31 18:45:15 +0530

    add 2020 job

---
+++ jobs/2020_success/broadcast_2020.py
@@ -0,0 +1,30 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.models import Variable
+from airflow.operators.python_operator import PythonOperator
+
+from config import local_tz, default_args
+from broadcaster.broadcast_2020_job import broadcast_2020
+
+broadcast_2020_cron = str(Variable.get("broadcast_2020_cron", '@yearly'))
+
+broadcast_2020_dag = DAG(
+    dag_id="broadcast_2020",
+    default_args=default_args,
+    schedule_interval=broadcast_2020_cron,
+    catchup=False,
+    start_date=datetime(year=2019, month=3, day=31, hour=0, minute=0, second=0,
+                        microsecond=0, tzinfo=local_tz),
+    dagrun_timeout=timedelta(minutes=1),
+)
+
+broadcast_2020_task = PythonOperator(
+    task_id="broadcast_2020",
+    task_concurrency=1,
+    python_callable=broadcast_2020,
+    dag=broadcast_2020_dag,
+    op_kwargs={},
+    pool="scheduled_jobs_pool",
+    retry_exponential_backoff=True
+)
---
+++ jobs/2020_success/broadcast_2020_job.py
@@ -0,0 +1,35 @@
+from airflow.models import Variable
+from airflow.utils.log.logging_mixin import LoggingMixin
+from common.helpers import process_dynamic_task_sql, send_chat_message_patient_id
+
+log = LoggingMixin().log
+
+
+def broadcast_2020():
+
+    process_broadcast_2020 = int(Variable.get(
+        'process_broadcast_2020', '0'))
+
+    if process_broadcast_2020 == 1:
+        return
+
+    pids = str(Variable.get("broadcast_2020_id"))
+    pids = pids.split(",")
+    log.debug(pids)
+
+    for pid in pids:
+        try:
+            action = "dynamic_message"
+            message = str(Variable.get("broadcast_2020_msg", ''))
+            message += pid
+            payload = {
+                "action": action,
+                "message": message,
+                "is_notification": False
+            }
+            send_chat_message_patient_id(pid, message)
+        except Exception as e:
+            warning_message = "Cannot send message"
+            log.warning(warning_message)
+            log.error(e, exc_info=True)
+            raise e
