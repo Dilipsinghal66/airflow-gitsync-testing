69ce9c96410573919e259b79da567f02da8bf9c4
arc1999 <ayushrajchauhan99@gmail.com>
2021-05-06 17:12:06 +0530

    feat: cc algo ZY-2473

--- broadcaster/broadcast_cc_algo_job.py
+++ broadcaster/broadcast_cc_algo_job.py
@@ -43,8 +43,9 @@ def latest_cc(patient_id ):
         collection = mongo_conn.get_database(
             "tracking").get_collection("cc_tracking")
 
-        results = collection.find({"patientId": patient_id},{"_id":0,"chiefComplaints.ongoing":1,"chiefComplaints.complaint":1}).sort("dateCreated",-1).limit(1)
+        results = collection.find_one({"patientId": patient_id},{"_id":0,"chiefComplaints.ongoing":1,"chiefComplaints.complaint":1}).sort("dateCreated",-1).limit(1)
         icds =[]
+        print(results)
         for q in results['chiefComplaints']:
             if q['ongoing']==true:
                 icds.append(q['complaint'])
