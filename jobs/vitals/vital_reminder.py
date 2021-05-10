c0ad64cdc73c1080eb9fdbb30b73053b29939d81
Aishwary Bhashkar <aishwaray@gmail.com>
2021-05-10 17:07:38 +0530

    fix: uncommented the send mmessage

--- jobs/vitals/vital_reminder.py
+++ jobs/vitals/vital_reminder.py
@@ -125,24 +125,24 @@ def send_vital_reminder_func():
                     message_to_send = pre_message + param_grp_id_dict[value] + custom_patient_id_dict[key]
                 else:
                     message_to_send = pre_message + param_grp_id_dict[value]
-                #action = "vitals_reminder_6_am"
+                action = "vitals_reminder_6_am"
                 log.info("patient_id " + str(key))
                 log.info("Message " + message_to_send)
-                #try:
-                #    patient_id_message_send(key, message_to_send, action)
-                #except Exception as e:
-                #    print("Error Exception raised")
-                #    print(e)
+                try:
+                    patient_id_message_send(key, message_to_send, action)
+                except Exception as e:
+                    print("Error Exception raised")
+                    print(e)
             if key in custom_patient_id_dict:
                 message_to_send = pre_message + custom_patient_id_dict[key]
-                #action = "vitals_reminder_6_am"
+                action = "vitals_reminder_6_am"
                 log.info("patient_id " + str(key))
                 log.info("Message " + message_to_send)
-                #try:
-                #    patient_id_message_send(key, message_to_send, action)
-                #except Exception as e:
-                #    print("Error Exception raised")
-                #    print(e)
+                try:
+                    patient_id_message_send(key, message_to_send, action)
+                except Exception as e:
+                    print("Error Exception raised")
+                    print(e)
 
     except Exception as e:
         print("Error Exception raised")
