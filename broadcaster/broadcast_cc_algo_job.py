67043e6b279639369d891816b729b4023d14fa61
arc1999 <ayushrajchauhan99@gmail.com>
2021-05-19 17:09:33 +0530

    fix:remove word ZY-2473

--- broadcaster/broadcast_cc_algo_job.py
+++ broadcaster/broadcast_cc_algo_job.py
@@ -88,5 +88,8 @@ def form_msg(salutation,surname,cc):
 
     msg = "Dear "+salut+" "+surname+" - The doctors would like to know how you are doing on the below health issues, please let me know which are better and which are same as before: "
     for c in cc:
-            msg +="<br>"+"⬤"+" "+c+" "
+        x = c.replace(", unspecified", "")
+        y = x.replace(" unspecified", "")
+        z = y.replace("unspecified ", "")
+        msg +="<br>"+"⬤"+" "+z+" "
     return msg
