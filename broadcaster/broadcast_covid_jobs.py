c37b5bcc57965b68a25befe3f3800de4f0e4a2de
Pranjal <pranjal9599@gmail.com>
2021-05-28 13:06:07 +0530

    fix: ZY-2671

--- broadcaster/broadcast_covid_jobs.py
+++ broadcaster/broadcast_covid_jobs.py
@@ -6,16 +6,17 @@ from common.db_functions import get_data_from_db
 from common.helpers import process_custom_message
 
 
-
 log = LoggingMixin().log
 
 
 def get_user_ids():
     try:
         mongo_conn = MongoHook(conn_id="mongo_prod").get_conn()
-        collection = mongo_conn.get_database("plan-service").get_collection("plan_assignments")
+        collection = mongo_conn.get_database(
+            "plan-service").get_collection("plan_assignments")
 
-        results = collection.find({"planid":31})
+        results = collection.find(
+            {"planid": {"$in": [31, 42, 44, 48, 49, 51, 53]}})
         patientIds = []
         for q in results:
             patientIds.append(q['patientid'])
@@ -25,8 +26,8 @@ def get_user_ids():
         connection = engine.get_conn()
         cursor = connection.cursor()
         userid_query = "select id from zylaapi.auth where phoneno in (select phoneno from " \
-                                      "patient_profile where id in (" + ','.join(str(x) for x in patientIds) \
-                                      + ")) and who = \'patient\'"
+            "patient_profile where id in (" + ','.join(str(x) for x in patientIds) \
+            + ")) and who = \'patient\'"
 
         cursor.execute(userid_query)
         user_ids = []
@@ -48,4 +49,3 @@ def broadcast_covid():
 
     message = str(Variable.get("broadcast_covid_msg", ""))
     process_custom_message(user_ids, message)
-
