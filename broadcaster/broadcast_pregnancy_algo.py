06b543d0ef8d629f9d72f66bd4c82435683bcccd
arc1999 <ayushrajchauhan99@gmail.com>
2021-06-24 15:17:16 +0530

    feat:pregnancy algo ZY-3002

---
+++ broadcaster/broadcast_pregnancy_algo.py
@@ -0,0 +1,30 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.models import Variable
+from airflow.operators.python_operator import PythonOperator
+
+from config import local_tz, default_args
+from broadcaster.broadcast_pregnancy_algo_job import broadcast_send_pregnancy_card
+
+broadcast_pregnancy_algo_cron = str(Variable.get("broadcast_pregnancy_algo_job", '@once'))
+
+broadcast_pregnancy_algo_dag = DAG(
+    dag_id="broadcast_pregnancy_algo",
+    default_args=default_args,
+    schedule_interval=broadcast_pregnancy_algo_cron,
+    catchup=False,
+    start_date=datetime(year=2019, month=3, day=31, hour=0, minute=0, second=0,
+                        microsecond=0, tzinfo=local_tz),
+    dagrun_timeout=timedelta(minutes=1),
+)
+
+broadcast_newuser_whatsapp_task = PythonOperator(
+    task_id="broadcast_pregnancy_algo",
+    task_concurrency=1,
+    python_callable=broadcast_send_pregnancy_card,
+    dag=broadcast_pregnancy_algo_dag,
+    op_kwargs={},
+    pool="scheduled_jobs_pool",
+    retry_exponential_backoff=True
+)
