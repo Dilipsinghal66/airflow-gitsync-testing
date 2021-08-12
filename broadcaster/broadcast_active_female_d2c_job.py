c425b69faee0d369d686416bc06b7d0805429be8
Aishwary Bhashkar <aishwaray@gmail.com>
2021-08-12 10:55:30 +0530

    feat: female d2c patients

---
+++ broadcaster/broadcast_active_female_d2c_job.py
@@ -0,0 +1,26 @@
+from airflow.models import Variable
+from airflow.utils.log.logging_mixin import LoggingMixin
+from common.helpers import process_custom_message_sql
+
+log = LoggingMixin().log
+
+"""
+All paid female patients on bridge (status = 4)
+"""
+
+
+def broadcast_active_female_d2c():
+
+    process_broadcast_active_females = int(Variable.get(
+        'process_broadcast_active_female_d2c_disable', '0'))
+
+    if process_broadcast_active_females == 1:
+        return
+
+    sql_query = str(Variable.get("broadcast_active_female_d2c_sql",
+                                 "select id from zylaapi.auth where who = \'patient\' and phoneno in (select phoneno "
+                                 "from zylaapi.patient_profile where gender = 1 and status = 4 and referred_by = 0 and "
+                                 "new_chat = 1)"))
+
+    message = str(Variable.get("broadcast_active_female_d2c_msg", ''))
+    #process_custom_message_sql(sql_query, message)
