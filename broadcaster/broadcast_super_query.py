ee0eb1af2a48ad7073e44762ec973dcf85e3d0f1
Aishwary Bhashkar <aishwaray@gmail.com>
2021-08-27 14:33:31 +0530

    feat: broadcast super query

---
+++ broadcaster/broadcast_super_query.py
@@ -0,0 +1,30 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.models import Variable
+from airflow.operators.python_operator import PythonOperator
+
+from config import local_tz, default_args
+from broadcaster.broadcast_super_query_jobs import broadcast_super_query
+
+broadcast_super_query_cron = str(Variable.get("broadcast_super_query_cron", '@yearly'))
+
+broadcast_super_query_dag = DAG(
+    dag_id="broadcast_super_query",
+    default_args=default_args,
+    schedule_interval=broadcast_super_query_cron,
+    catchup=False,
+    start_date=datetime(year=2019, month=3, day=31, hour=0, minute=0, second=0,
+                        microsecond=0, tzinfo=local_tz),
+    dagrun_timeout=timedelta(minutes=1),
+)
+
+broadcast_super_query_task = PythonOperator(
+    task_id="broadcast_active",
+    task_concurrency=1,
+    python_callable=broadcast_super_query,
+    dag=broadcast_super_query_dag,
+    op_kwargs={},
+    pool="scheduled_jobs_pool",
+    retry_exponential_backoff=True
+)
