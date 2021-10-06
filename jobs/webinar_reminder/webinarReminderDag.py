2900d296f6d10407edd2b71133a49e62f4907484
Pranjal S <pranjals@Pranjals-MacBook-Pro.local>
2021-10-06 12:34:02 +0530

    feat: ZY-3768

--- jobs/webinar_reminder/webinarReminderDag.py
+++ jobs/webinar_reminder/webinarReminderDag.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from jobs.webinar_reminder.webinarReminderJob import initializer
+from jobs.webinar_reminder.webinarReminderJob import initializer,interaktJob
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from config import default_args, local_tz
@@ -42,6 +42,25 @@ webinar_reminder_task_one_day_before = PythonOperator(
     python_callable=initializer,
     dag=webinar_reminder_dag_one_day_before,
     op_kwargs={"rType": 2},
+    pool="scheduled_jobs_pool",
+    retry_exponential_backoff=True,
+    provide_context=True
+)
+
+
+webinar_reminder_interakt_dag = DAG(
+    dag_id="webinar_reminder_interakt_dag",
+    start_date=datetime(year=2020, month=2, day=3, hour=9, minute=0, second=0,
+                        microsecond=0, tzinfo=local_tz),
+    schedule_interval="00 12 * * *",
+    catchup=False
+)
+
+webinar_reminder_interakt_task = PythonOperator(
+    task_id="webinar_reminder_interakt_dag",
+    task_concurrency=1,
+    python_callable=interaktJob,
+    dag=webinar_reminder_interakt_dag,
     pool="scheduled_jobs_pool",
     retry_exponential_backoff=True,
     provide_context=True
