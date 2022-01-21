9b453f2a76a0f32b4b14a4c6bce41b9cfeff232b
Aishwary Bhashkar <aishwaray@gmail.com>
2022-01-21 14:28:24 +0530

    ZY-4451 broad cast query for heart

--- broadcaster/broadcast_active_az_heart_job.py
+++ broadcaster/broadcast_active_az_heart_job.py
@@ -7,23 +7,23 @@ from common.helpers import process_custom_message_sql
 log = LoggingMixin().log
 
 
-def broadcast_active_kidney():
+def broadcast_active_az_heart():
 
-    process_broadcast_active_kidney = int(Variable.get("process_broadcast_active_kidney", '0'))
-    if process_broadcast_active_kidney == 1:
+    process_broadcast_active_az_heart = int(Variable.get("process_broadcast_active_az_heart", '0'))
+    if process_broadcast_active_az_heart == 1:
         return
 
     mongo_conn = MongoHook(conn_id="mongo_prod").get_conn()
     collection = mongo_conn.get_database("plan-service").get_collection("plan_assignments")
 
-    results = collection.find({'planid': 54})
+    results = collection.find({'planid': 63})
     patientIds = []
     for q in results:
         patientIds.append(q['patientid'])
 
-    filter_active_patient_query = "select id from patient_profile where status=4 and new_chat=1 and id in (" + ','.join(
-        str(x) for x in patientIds) + ")"
+    filter_active_patient_query = "select id from patient_profile where status = 4 and client_code = 'AZ' and " \
+                                  "new_chat = 1 and id in (" + ','.join(str(x) for x in patientIds) + ")"
     log.info(filter_active_patient_query)
 
-    message = str(Variable.get("broadcast_active_kidney_msg", ''))
+    message = str(Variable.get("broadcast_active_az_heart_msg", ''))
     #process_custom_message_sql(filter_active_patient_query, message)
