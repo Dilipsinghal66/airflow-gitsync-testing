fd6606fd88721d2c74ebefa8e6729f81d164d33f
zyla <aishwaray@gmail.com>
2022-06-20 11:00:56 +0530

    ZY-4347 updated query for custom message

--- broadcaster/broadcast_active_insulin_job.py
+++ broadcaster/broadcast_active_insulin_job.py
@@ -4,7 +4,7 @@ from airflow.contrib.hooks.mongo_hook import MongoHook
 from pymongo.collection import Collection
 from pymongo.cursor import Cursor
 from common.db_functions import get_data_from_db
-from common.helpers import patient_id_message_send
+from common.helpers import process_custom_message_user_id
 from datetime import datetime
 
 
@@ -54,11 +54,25 @@ def broadcast_active_insulin():
     group_id = "broadcast_active_insulin " + date_string
 
     message = str(Variable.get("broadcast_active_insulin_msg", ""))
+    engine = get_data_from_db(db_type="mysql", conn_id="mysql_monolith")
+    connection = engine.get_conn()
+    cursor = connection.cursor()
     for patient_id in patientIds:
         if message:
             try:
-                patient_id_message_send(patient_id, message, "dynamic_message", group_id)
-                print(patient_id)
+
+                user_id_sql_query = "select id from zylaapi.auth where phoneno = " \
+                                    "(select phoneno from zylaapi.patient_profile" \
+                                    " where id = " + str(patient_id) + ") and countrycode = (select countrycode " \
+                                                                       "from zylaapi.patient_profile where id = " + \
+                                    str(patient_id) + ") and who = 'patient' "
+                cursor.execute(user_id_sql_query)
+                user_id = 0
+                for row in cursor.fetchall():
+                    user_id = row[0]
+                log.info("sending for user id " + str(user_id))
+                if user_id != 0:
+                    process_custom_message_user_id(user_id, message, "", group_id)
             except Exception as e:
                 print("Error Exception raised")
                 print(e)
