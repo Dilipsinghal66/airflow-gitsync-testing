1ce0f2b2b5e661605d2f31f345303bce48fc4fec
zyla <aishwaray@gmail.com>
2022-11-14 15:28:54 +0530

    chore: test message

--- jobs/html_message_test/html_message_test_job.py
+++ jobs/html_message_test/html_message_test_job.py
@@ -10,15 +10,19 @@ log = LoggingMixin().log
 
 def send_test_message(**kargs):
     html_test_message = Variable.get('html_test_message_dag_msg', None)
-    html_test_patient_id = Variable.get('html_test_patient_id', None)
+    #html_test_patient_id = Variable.get('html_test_patient_id', None)
 
+    html_test_patient_id = 18645
     if not html_test_message or not html_test_patient_id:
         raise ValueError("Config variables not defined")
 
+    group_id = "send_test_message"
+
     payload = {
-        "action": "custom_message",
+        "action": "dynamic_message",
         "message": str(html_test_message),
-        "is_notification": False
+        "is_notification": False,
+        "groupId": group_id
     }
 
     try:
