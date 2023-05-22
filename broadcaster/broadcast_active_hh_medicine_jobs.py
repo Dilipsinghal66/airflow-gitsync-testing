3c376282aa4a0e8fac0a70dda87c056dbd1f2a27
Abhinav Singh Upperwal <abhinav@zyla.in>
2023-05-22 17:21:26 +0530

    feat:broadcast_active_hh_medicine_msg correction_3

--- broadcaster/broadcast_active_hh_medicine_jobs.py
+++ broadcaster/broadcast_active_hh_medicine_jobs.py
@@ -68,14 +68,25 @@ def broadcast_active_hh_medicine():
     date_string = f'{datetime.now():%Y-%m-%d %H:%M:%S%z}'
     group_id = "broadcast_active_hh_medicine" + date_string
     message = str(Variable.get("broadcast_active_hh_medicine_msg", ""))
-    
-    user_id = 0
-    for user_id in patientIds:
+    engine = get_data_from_db(db_type="mysql", conn_id="mysql_monolith")
+    connection = engine.get_conn()
+    cursor = connection.cursor()
+    for patient_id in patientIds:
         if message:
             try:
+
+                user_id_sql_query = "select id from zylaapi.auth where phoneno = " \
+                                    "(select phoneno from zylaapi.patient_profile" \
+                                    " where id = " + str(patient_id) + ") and countrycode = (select countrycode " \
+                                                                       "from zylaapi.patient_profile where id = " + \
+                                    str(patient_id) + ") and who = 'patient' "
+                cursor.execute(user_id_sql_query)
+                user_id = 0
+                for row in cursor.fetchall():
+                    user_id = row[0]
                 log.info("sending for user id " + str(user_id))
                 if user_id != 0:
-                        process_custom_message_user_id(user_id, message, "", group_id)
+                    process_custom_message_user_id(user_id, message, "", group_id)
             except Exception as e:
                 print("Error Exception raised")
                 print(e)
